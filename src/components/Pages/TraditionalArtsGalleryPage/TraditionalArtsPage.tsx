4f323c3b87a4f0b81aad3b224ed061da60d0dc5b
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-10-30 14:23:11 +0000

    Remove placeholder link button from traditional arts page

--- src/components/Pages/TraditionalArtsGalleryPage/TraditionalArtsPage.tsx
+++ src/components/Pages/TraditionalArtsGalleryPage/TraditionalArtsPage.tsx
@@ -9,8 +9,6 @@ const TraditionalArtsPage: React.FC = () => {
         title='Traditional Arts'
         content='I really enjoy working with watercolors and doing traditional art. I also like to experiment with combining different media. I enjoy live drawing, capturing things in the moment.'
         imageData={imageData}
-        routeLink='test'
-        buttonLabelLink='For more you can look at my instargram'
       />
     </>
   )
